5bc94122abf6a6ac12d165a435215a4a511d18a9
S. van Laere <svanlaere@users.noreply.github.com>
2025-07-02 23:18:37 +0200

    Update index.php
    - Strict typing
    - HTML 5 Doctype

--- wolf/install/index.php
+++ wolf/install/index.php
@@ -1,5 +1,8 @@
 <?php
-    /*
+
+declare(strict_types=1);
+
+/*
  * Wolf CMS - Content Management Simplified. <http://www.wolfcms.org>
  * Copyright (C) 2009-2013 Martijn van der Kleijn <martijn.niji@gmail.com>
  *
@@ -7,83 +10,85 @@
  * Please see license.txt for the full license text.
  */
 
-    /**
-     * @package Installer
-     */
+/**
+ * @package Installer
+ */
 
-    // Make sure we hide ugly errrors
-    error_reporting(error_level: 0);
+// Make sure we hide ugly errrors
+error_reporting(error_level: 0);
 
-    define(constant_name: 'INSTALL_SEQUENCE', value: true);
+define(constant_name: 'INSTALL_SEQUENCE', value: true);
 
-    define(constant_name: 'CORE_ROOT', value: dirname(path: __DIR__));
-    define(constant_name: 'CFG_FILE', value: '../../config.php');
-    define(constant_name: 'PUBLIC_ROOT', value: '../../public/');
-    define(constant_name: 'DEFAULT_ADMIN_USER', value: 'admin');
-    require_once CORE_ROOT . '/Framework.php';
+define(constant_name: 'CORE_ROOT', value: dirname(path: __DIR__));
+define(constant_name: 'CFG_FILE', value: '../../config.php');
+define(constant_name: 'PUBLIC_ROOT', value: '../../public/');
+define(constant_name: 'DEFAULT_ADMIN_USER', value: 'admin');
+require_once CORE_ROOT . '/Framework.php';
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-    <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+    <meta charset="utf-8">
     <title>Wolf CMS - Install/Upgrade routine</title>
-    <link href="install.css" media="screen" rel="Stylesheet" type="text/css" />
+    <link href="install.css" rel="stylesheet" type="text/css" media="screen">
 </head>
+
 <body>
     <div id="header">
         <div id="site-title">Wolf CMS</div>
     </div>
     <div id="content">
 
-<?php
-    // PHP 8.3 Refactor
+        <?php
+        // PHP 8.3 Refactor
+        
+        // Check if config file exists and has content
+        function isConfigFileValid(): bool
+        {
+            return file_exists(CFG_FILE) && filesize(CFG_FILE) > 1;
+        }
 
-    // Check if config file exists and has content
-    function isConfigFileValid(): bool
-    {
-        return file_exists(CFG_FILE) && filesize(CFG_FILE) > 1;
-    }
+        // Using match expression for clearer intent
+        $action = match (true) {
+            isset($_POST['install']) && !isset($_POST['commit']) && !isConfigFileValid() => 'install',
+            isset($_POST['install'], $_POST['commit'], $_POST['config']) => 'do_install',
+            isset($_POST['upgrade'], $_POST['commit']) && isConfigFileValid() => 'do_upgrade',
+            !isset($_POST['upgrade'], $_POST['commit']) && isConfigFileValid() => 'upgrade',
+            default => 'requirements',
+        };
 
-    // Using match expression for clearer intent
-    $action = match (true) {
-        isset($_POST['install']) && ! isset($_POST['commit']) && ! isConfigFileValid() => 'install',
-        isset($_POST['install'], $_POST['commit'], $_POST['config']) => 'do_install',
-        isset($_POST['upgrade'], $_POST['commit']) && isConfigFileValid() => 'do_upgrade',
-        ! isset($_POST['upgrade'], $_POST['commit']) && isConfigFileValid() => 'upgrade',
-        default => 'requirements',
-    };
+        switch ($action) {
+            case 'install':
+                require_once 'install.php';
+                break;
 
-    switch ($action) {
-        case 'install':
-            require_once 'install.php';
-            break;
+            case 'do_install':
+                $config = $_POST['config'];
+                require_once 'do-install.php';
+                require_once 'post-install.php';
+                break;
 
-        case 'do_install':
-            $config = $_POST['config'];
-            require_once 'do-install.php';
-            require_once 'post-install.php';
-            break;
+            case 'do_upgrade':
+                require_once CFG_FILE;
+                require_once CORE_ROOT . '/Framework.php';
+                require_once 'do-upgrade.php';
+                break;
 
-        case 'do_upgrade':
-            require_once CFG_FILE;
-            require_once CORE_ROOT . '/Framework.php';
-            require_once 'do-upgrade.php';
-            break;
+            case 'upgrade':
+                require_once 'upgrade.php';
+                break;
 
-        case 'upgrade':
-            require_once 'upgrade.php';
-            break;
-
-        default:
-            require_once 'requirements.php';
-            break;
-    }
-?>
+            default:
+                require_once 'requirements.php';
+                break;
+        }
+        ?>
 
     </div>
     <div id="footer">
         <p>Powered by <a href="http://www.wolfcms.org/">Wolf CMS</a></p>
     </div>
 </body>
+
 </html>
